396528ead6a97dfcf73fb406d9725c8996e39d0b
Munkhjargal <munkhjargalbatdavaa@gmail.com>
2021-05-08 00:32:33 +0800

    created ListItems and items div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import ListItems from "./ListItems";
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faTrash);
 
 class App extends React.Component{
 
@@ -14,6 +19,38 @@ class App extends React.Component{
         text: "",
         key: ""
       }
+    }
+    // binding, constructor iin this iig ashiglah  
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  // Defining handleInput method (inputiig avah)
+  handleInput(e){
+    // changing state
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  // Defining addItem method (add item)
+  addItem(e){
+    e.preventDefault(); // button daragdhad page refreshdeh default iig arilgah
+    const newItem = this.state.currentItem;
+    // console.log(newItem);
+
+    if(newItem.text !== ""){
+      const newItems = [...this.state.items, newItem]; // listlegdsn itemudig tusad ni bolgoj salgah
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text: "",
+          key: ""
+        }
+      })
     }
   }
 
@@ -36,7 +73,7 @@ class App extends React.Component{
 
         </form>
       </header>
-
+      <ListItems items = {this.state.items}></ListItems>
       </div>
     );
   }
---
+++ src/ListItems.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ListItems.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(items => 
+        {
+            // item tus buriin div
+            return(
+                <div className = "list" key = "item.key">
+                    <p>{items.text}
+                    <span>
+                        <FontAwesomeIcon className = "faicons" icon = "trash"/>
+                    </span>
+                    </p>
+                    
+                </div>
+            )
+        })
+
+        return(
+            <div>{listItems}</div>
+        )
+   
+}
+
+export default ListItems;
