745dde0ebd8c2dad56e3d38c82637b94f7efc1e7
Munkhjargal <munkhjargalbatdavaa@gmail.com>
2021-05-07 22:23:01 +0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component{
+
+  // Baiguulagch funkts
+  constructor(props){
+    super(props); // base class
+
+    // tuluviig todorhoiloh
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    }
+  }
+
+  // Rendering
+  render(){
+    return(
+      <div className = "App">
+
+      <header>        
+        <form id="todoForm" onSubmit={this.addItem} /* click hiigdeh uyd duudagdana*/ >
+
+          <input 
+          type="text" 
+          placeholder = "What do you want to do ?" 
+          value={this.state.currentItem.text} /* CurrentItem Input field 2iig holboh */
+          onChange={this.handleInput} // handling userinpunt in realtime
+          />
+
+          <button type="submit">Add</button>
+
+        </form>
       </header>
-    </div>
-  );
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
